bef90d8f6105f96367a8eae8e5769abb85d2ea53
Banzi Dube <banzidube8@gmail.com>
2025-07-17 14:50:11 +0200

    img edits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const serviceImages = [
-  "/images/clean.jpg",
-  "/images/Plumber.jpg",
-  "/images/electrician-working.jpg",
-  "/images/landscape-gardener.jpg",
-  "/images/image-asset.jpg",
+  "clean.jpg",
+  "Plumber.jpg",
+  "electrician-working.jpg",
+  "landscape-gardener.jpg",
+  "image-asset.jpg",
 ];
 
 // Slide Show Settings
